618e7f30495e876f09e7dcf1bbfe3769852abb1b
15031777 <15031777@myrp.edu.sg>
2017-05-18 14:46:42 +0800

    insert

--- app/src/main/java/com/example/a15017082/p05_ndpsongs/ShowSong.java
+++ app/src/main/java/com/example/a15017082/p05_ndpsongs/ShowSong.java
@@ -2,12 +2,30 @@ package com.example.a15017082.p05_ndpsongs;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 public class ShowSong extends AppCompatActivity {
+
+    ArrayList<String> al;
+    ArrayAdapter aa;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_show_song);
+
+        ListView lv = (ListView) findViewById(R.id.lv);
+
+        al = new ArrayList<String>();
+        aa = new ArrayAdapter<String>(this,
+                android.R.layout.simple_list_item_1, al);
+        lv.setAdapter(aa);
+
+
+
+
     }
 }
