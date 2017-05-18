362cb3c74aaf4f30451d49bb8afb3edc4debf94d
15017082 <15017082@myrp.edu.sg>
2017-05-18 13:30:39 +0800

    Juble

--- app/src/main/java/com/example/a15017082/p05_ndpsongs/Song.java
+++ app/src/main/java/com/example/a15017082/p05_ndpsongs/Song.java
@@ -12,7 +12,7 @@ public class Song {
     private int year;
     private int stars;
 
-    public Song(int id, String title, String singers, int year, int stars){
+    public Song(String title, String singers, int year, int stars){
         this.id = id;
         this.title = title;
         this.singers = singers;
