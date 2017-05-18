fed822b94f0df04177af586b2a1c6737c0aeb0d1
15017082 <15017082@myrp.edu.sg>
2017-05-18 13:29:20 +0800

    Juble

---
+++ app/src/main/java/com/example/a15017082/p05_ndpsongs/Song.java
@@ -0,0 +1,43 @@
+package com.example.a15017082.p05_ndpsongs;
+
+/**
+ * Created by 15017082 on 18/5/2017.
+ */
+
+public class Song {
+
+    private int id;
+    private String title;
+    private String singers;
+    private int year;
+    private int stars;
+
+    public Song(int id, String title, String singers, int year, int stars){
+        this.id = id;
+        this.title = title;
+        this.singers = singers;
+        this.year = year;
+        this.stars = stars;
+    }
+
+    public int get_Id(){
+        return id;
+    }
+
+    public String getTitle(){
+        return title;
+    }
+
+    public String getSingers(){
+        return singers;
+    }
+
+    public int getYear(){
+        return year;
+    }
+
+    public int getStar(){
+        return stars;
+    }
+}
+
