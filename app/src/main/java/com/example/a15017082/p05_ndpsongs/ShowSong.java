544e9a05a8e5c393da56f18b9b3c8f576517d91f
15031777 <15031777@myrp.edu.sg>
2017-05-18 14:32:52 +0800

    Merge branch 'master' of D:\Android\P05-NDPSongs with conflicts.

--- app/src/main/java/com/example/a15017082/p05_ndpsongs/ShowSong.java
+++ app/src/main/java/com/example/a15017082/p05_ndpsongs/ShowSong.java
@@ -3,11 +3,11 @@ package com.example.a15017082.p05_ndpsongs;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class secondActivity extends AppCompatActivity {
+public class ShowSong extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_second);
+        setContentView(R.layout.activity_show_song);
     }
 }
