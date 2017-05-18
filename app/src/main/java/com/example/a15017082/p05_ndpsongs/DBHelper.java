2113766b7cb69b7b3c5c6413bcd96db97d4581a7
15017082 <15017082@myrp.edu.sg>
2017-05-18 13:36:20 +0800

    Juble

--- app/src/main/java/com/example/a15017082/p05_ndpsongs/DBHelper.java
+++ app/src/main/java/com/example/a15017082/p05_ndpsongs/DBHelper.java
@@ -1,8 +1,49 @@
 package com.example.a15017082.p05_ndpsongs;
 
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+import android.util.Log;
+
 /**
  * Created by 15017082 on 18/5/2017.
  */
 
-public class DBHelper {
+public class DBHelper extends SQLiteOpenHelper {
+
+    //TODO Define the Database properties
+    private static final String DATABASE_NAME = "songs.db";
+    private static final int DATABASE_VERSION = 1;
+
+    private static final String TABLE_NOTE = "songs";
+    private static final String COLUMN_ID = "id";
+    private static final String COLUMN_TITLE = "title";
+    private static final String COLUMN_SINGERS = "singers";
+    private static final String COLUMN_YEAR = "year";
+    private static final String COLUMN_STARS = "stars";
+
+    public DBHelper(Context context) {
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        //TODO CREATE TABLE Note
+        String createTableSql = "CREATE TABLE " + TABLE_NOTE + "("
+                + COLUMN_ID + " INTEGER PRIMARY KEY AUTOINCREMENT,"
+                + COLUMN_TITLE + " TEXT,"
+                + COLUMN_SINGERS + " TEXT,"
+                + COLUMN_YEAR + " INT,"
+                + COLUMN_STARS + " INT )";
+        db.execSQL(createTableSql);
+        Log.i("info", "created tables");
+
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        db.execSQL("DROP TABLE IF EXISTS " + TABLE_NOTE);
+        onCreate(db);
+    }
+
 }
